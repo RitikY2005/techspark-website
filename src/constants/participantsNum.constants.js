6a48b55ad82b68361c3778dc5c25a70600f8d9ce
Ritikesh Yadav <131939663+RitikY2005@users.noreply.github.com>
2025-02-12 19:00:40 +0530

    added new participantsNum.constants.js

---
+++ src/constants/participantsNum.constants.js
@@ -0,0 +1,45 @@
+const participantsNum={
+  "Techspark Hackathon":{
+    min:3,
+    max:4
+  },
+  "Code - a - Replica":{
+    min:2,
+    max:2
+  },
+  "Escape Room":{
+    min:3,
+    max:3
+  },
+  "Treasure Hunt":{
+    min:3,
+    max:3
+  },
+  "Debate":{
+    min:1,
+    max:1
+  },
+  "Trivia":{
+    min:2,
+    max:2
+  },
+  "Battle Grounds Mobile India (BGMI)":{
+    min:4,
+    max:4
+  },
+  "Ultimate Goal Gala (FC24)":{
+    min:1,
+    max:1
+  },
+  "Netflicks and Chill (Football Tournament)":{
+    min:5,
+    max:7
+  },
+  "Queen's Gambit (Chess Tournament)":{
+    min:1,
+    max:1
+  }
+};
+
+
+export default participantsNum;
